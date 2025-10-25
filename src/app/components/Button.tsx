0377d21629ff37e51704c8b21000466a367b7941
dorawengg <cw749@cornell.edu>
2025-10-25 16:02:09 -0400

    change button param type for deployment

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
-function Button({content}: {content: String}) {
+function Button({ content }: { content: string }) {
 
 
   return (
-   <div>
-        <div className='bg-brand-orange w-1/2 md:w-1/5 p-4 m-2 mt-9 shadow-sm text-center rounded-xl text-white text-md md:text-lg font-bold transition duration-200 hover:bg-[#dc5d36]'>
-            <h1>{content}</h1>
-        </div>
-   </div>
+    <div>
+      <div className='bg-brand-orange w-1/2 md:w-1/5 p-4 m-2 mt-9 shadow-sm text-center rounded-xl text-white text-md md:text-lg font-bold transition duration-200 hover:bg-[#dc5d36]'>
+        <h1>{content}</h1>
+      </div>
+    </div>
   )
 }
 
