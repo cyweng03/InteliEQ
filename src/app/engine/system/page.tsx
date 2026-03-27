266f89e89ca5c691878dde12eab8b71dac854200
Enaika Kishnani <ek756@cornell.edu>
2026-03-27 17:27:14 -0400

    Added borders to team in about us, fixed icons and banner image resolution

--- src/app/engine/system/page.tsx
+++ src/app/engine/system/page.tsx
@@ -141,6 +141,8 @@ export default function FullStackPage() {
                 fill="none"
                 stroke="currentColor"
                 strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               >
                 <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
                 <path d="M14.8 9a2 2 0 0 0 -1.8 -1h-2a2 2 0 1 0 0 4h2a2 2 0 1 1 0 4h-2a2 2 0 0 1 -1.8 -1" />
